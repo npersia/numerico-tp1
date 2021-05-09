91c2b9024cd3625563fb011c82a19bd2c8793a75
nahuel <persianahuel@gmail.com>
2021-05-09 03:18:32 -0300

    metodo de biseccion

--- main.py
+++ main.py
@@ -3,10 +3,20 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+def cuadrado(x):
+    return x ** 2
+
+def raiz_cuadrada(x):
+    return x ** 0.5
+
+def operar(func, *args):
+    for n in args:
+        print(func(n))
+
 
 def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+    print(f'Hi, {name}')
+    operar(raiz_cuadrada, 9, 25, 64, 49)
 
 if __name__ == '__main__':
     print_hi('PyCharm')
