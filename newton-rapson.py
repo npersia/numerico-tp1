f34dd76109927a939feaf8b329624f32172c05ab
nahuel <persianahuel@gmail.com>
2021-05-09 04:11:43 -0300

    subo todo n_r

--- newton-rapson.py
+++ newton-rapson.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 
 
-def pivote(p,func,func_derivada):
-    return (p) - ( (func(p)) / (func_derivada(p)) )
+def pivote(p,f,f_derivada):
+    return (p) - ( f / f_derivada)
 
 def bolzano(x,y):
     return (x * y) < 0
@@ -16,38 +16,41 @@ def paro(x,y,e):
 
 
 
-def n_r(semilla,func,func_derivada,tolerancia):
-    p = pivote(semilla , func, func_derivada)
+def n_r(p,func,func_derivada,tolerancia):
     f_p = func(p)
+    f_derivada_p = func_derivada(p)
 
 
-    df = pd.DataFrame(columns=['an', 'bn', 'pn', 'f(pn)'])
-    df = df.append({'an': a, 'bn': b, 'pn': p, 'f(pn)': f_p}, ignore_index=True)
+    df = pd.DataFrame(columns=['pn', 'f(pn)'])
+    df = df.append({'pn': p, 'f(pn)': f_p}, ignore_index=True)
 
-    if f_a == 0:
-        return df
-    if f_b == 0:
-        return df
     if f_p == 0:
         return df
 
+
     parar = False
     while not parar:
-        if bolzano(f_p,f_a):
-            b = p
-        else:
-            a = p
-            f_a = f_p
 
         p_ant = p
 
-        p = pivote(a, b)
+        p = pivote(p, f_p, f_derivada_p)
         f_p = func(p)
+        f_derivada_p = func_derivada(p)
 
-        df = df.append({'an': a, 'bn': b, 'pn': p, 'f(pn)': f_p}, ignore_index=True)
+        df = df.append({'pn': p, 'f(pn)': f_p}, ignore_index=True)
+
         if f_p == 0:
             return df
 
         parar = paro(p, p_ant, tolerancia)
 
     return df
+
+
+def f(x):
+    return (x**3)+(4*(x**2))-(10)
+
+def f_der(x):
+    return 3*(x**2)+(8*(x))
+
+print(n_r(10,f,f_der,1E-4))
