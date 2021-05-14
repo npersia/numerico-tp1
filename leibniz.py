6aef125407611d07b72473fdfde2ba0d9efbd12e
nahuel <persianahuel@gmail.com>
2021-05-14 01:08:40 -0300

    agrego el metodo de leibniz

--- leibniz.py
+++ leibniz.py
@@ -2,17 +2,37 @@ import pandas as pd
 import numpy as np
 
 
-def leibniz(n):
 
+def prec64(n):
+    return np.float64(n)
+
+def prec32(n):
+    return np.float32(n)
+
+def prec(p):
+    if p == 32:
+        return prec32
+    if p == 64:
+        return prec64
+
+def leibniz(n,p=64):
+
+    precision = prec(p)
     par = 1
     sum = 0
     for x in range(n+1):
-        sum = sum + ((par * 1)/((2 * x) + 1))
+        sum = precision(precision(sum) + precision(par * 1/precision(precision(2 * x) + 1)))
 
         par = par * -1
 
+    return precision(sum)
 
-    return sum
 
 
-print(4*leibniz(100000))
+
+
+
+
+
+print(4*leibniz(1000000,p=32))
+print(4*leibniz(1000000,p=64))
--- newton-rapson.py
+++ newton-rapson.py
@@ -1,22 +1,37 @@
 import pandas as pd
 import numpy as np
 
+def prec64(n):
+    return np.float64(n)
+
+def prec32(n):
+    return np.float32(n)
+
+def prec(p):
+    if p == 32:
+        return prec32
+    if p == 64:
+        return prec64
 
 
-def pivote(p,f,f_derivada):
-    return (p) - ( f / f_derivada)
+def pivote(p,f,f_derivada,pre=64):
+    precision = prec(pre)
+    return precision(p) - ( precision(f)/ precision(f_derivada))
 
-def paro(x,y,e):
-    if x != 0:
-        return ( abs(x-y)/abs(x) ) < e
+def paro(x,y,e,pre=64):
+    precision = prec(pre)
+    if x != 0 :
+        return ( abs(precision(x)-precision(y))/abs(precision(x)) ) < precision(e)
     else:
-        return (abs(x-y) < e)
+        return (abs(precision(x)-precision(y)) < precision(e))
 
 
 
-def n_r(p,func,func_derivada,tolerancia):
-    f_p = func(p)
-    f_derivada_p = func_derivada(p)
+def n_r(p,func,func_derivada,tolerancia,pre=64):
+    precision = prec(pre)
+
+    f_p = precision(func(p))
+    f_derivada_p = precision(func_derivada(p))
 
 
     df = pd.DataFrame(columns=['pn', 'f(pn)'])
@@ -31,16 +46,16 @@ def n_r(p,func,func_derivada,tolerancia):
 
         p_ant = p
 
-        p = pivote(p, f_p, f_derivada_p)
-        f_p = func(p)
-        f_derivada_p = func_derivada(p)
+        p = precision(pivote(p, f_p, f_derivada_p,pre))
+        f_p = precision(func(p))
+        f_derivada_p = precision(func_derivada(p))
 
         df = df.append({'pn': p, 'f(pn)': f_p}, ignore_index=True)
 
         if f_p == 0:
             return df
 
-        parar = paro(p, p_ant, tolerancia)
+        parar = precision(paro(p, p_ant, tolerancia,pre))
 
     return df
 
@@ -61,3 +76,4 @@ def coseno(x):
 #print(n_r(2,f,f_der,1E-4))
 
 print(n_r(2,seno,coseno,1E-10))
+print(n_r(2,seno,coseno,1E-10,32))
